a50746af22c40cc757ef0b59f8f3ae8a8b05271b
Calime Silva <55805944+CalimeSilva@users.noreply.github.com>
2022-05-01 23:24:20 +0100

    Update valiacao.php

--- valiacao.php
+++ valiacao.php
@@ -1,6 +1,12 @@
 <?php
-$erro = false;
-if ( !isset( $_POST ) || empty( $_POST ) ) {
-	$erro = 'Nada foi postado.';
-}
->
+
+	class Validacao
+	{
+		public function NomeValidar($nomePost)
+		{
+			$nome = $_POST[$nomePost];
+			if (!preg_match("/^[a-zA-Z-' ]*$/", $nome)) {
+				$nameErr = "Only letters and white space allowed";
+			}
+		}
+	}
